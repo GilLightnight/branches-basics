9232aa97634ebd3243c0007ed7bb2ee9ce41ea44
Gil Lightnight <kosykh.igor11@gmail.com>
2020-03-17 12:34:18 +0700

    Added format for text

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-	printf("I am the Bone of my Sword. Steel is my Body and Fire is my Blood. I have created over a Thousand Blades. Unknown to Death. Nor known to Life. Have withstood Pain to create many Weapons. Yet those Hands will never hold Anything. So as I pray-- Unlimited Blade Works!");
+	printf("I am the Bone of my Sword.\n"
+		"Steel is my Body and Fire is my Blood.\n"
+	       	"I have created over a Thousand Blades.\n"
+	       	"Unknown to Death.\n"
+	       	"Nor known to Life.\n"
+	       	"Have withstood Pain to create many Weapons.\n"
+	       	"Yet those Hands will never hold Anything.\n"
+	       	"So as I pray--\n"
+	       	"Unlimited Blade Works!\n");
 	return 0;
 }
