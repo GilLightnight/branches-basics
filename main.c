e26eed1e143cb75f172f6133715577112c6610f1
Gil Lightnight <kosykh.igor11@gmail.com>
2020-03-17 12:26:03 +0700

    Remade printf

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-	printf("I am the bone of my sword. So as I pray Unlimited Blade Works!");
+	printf("I am the Bone of my Sword. Steel is my Body and Fire is my Blood. I have created over a Thousand Blades. Unknown to Death. Nor known to Life. Have withstood Pain to create many Weapons. Yet those Hands will never hold Anything. So as I pray-- Unlimited Blade Works!");
 	return 0;
 }
